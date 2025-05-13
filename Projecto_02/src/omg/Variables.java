f58c8520b5dfd223e2c5942b272233ca02602ebd
Fernando <124049519@upq.edu.mx>
2025-05-13 08:42:13 -0600

    Se creo un archivo nuevo, y se creo primer programa en la IDE de java

---
+++ Projecto_02/src/omg/Variables.java
@@ -0,0 +1,32 @@
+package omg;
+
+public class Variables {
+	public static void main (String[] args)throws Exception{
+		int numero1 = 10;
+		double numero2;
+		numero2 = 3.14D; //Se pone la D o d al final al declarar un double
+		float numero3;
+		numero3=3.14159F; //Se tiene que poner una F o f de float al final
+		char caracter1, caracter2;
+		caracter1='A'; //Comillas simples.
+		caracter2=64;
+		boolean opcion=true;
+		long numero4;
+		numero4=987654321L; //Poner una L o l al final, para el Long
+		byte numero5;
+		numero5=126;
+		short numero6;
+		numero6=128;
+		
+		System.out.println(numero1);
+		System.out.println(numero2); 
+		System.out.println(numero3);
+		System.out.println(numero4);
+		System.out.println(numero5);
+		System.out.println(numero6);
+		System.out.println(caracter1);
+		System.out.println(caracter2);
+		System.out.println(opcion);
+	}
+
+}
